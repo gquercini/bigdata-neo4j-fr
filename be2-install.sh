ad4dd4070710a11e1ac3daf44cef8348cff6ed0a
Gianluca Quercini <gquercini@users.noreply.github.com>
2017-12-13 16:20:15 +0100

    Update be2-install.sh

--- be2-install.sh
+++ be2-install.sh
@@ -4,28 +4,26 @@
 # Author: Gianluca Quercini
 # Year : 2017 - 2018
 
+
+echo -e "\n******\n\nFichier d'installation du BE 2 du cours Big Data.\nAuteur : Gianluca QUERCINI\nAnnée : 2017 - 2018\n\n******\n"
+
 wdir='bigdata-be2'
-cd ~
-if [ -d $wdir ]; then 
+if [ -d ~/$wdir ]; then 
   echo -e "Elimination des anciens fichiers d'installation"
-  ./$wdir/neo4j-community-2.3.7/bin/neo4j stop  > /dev/null 2>&1
-  rm -rf ./$wdir
+  ~/$wdir/neo4j-community-2.3.7/bin/neo4j stop  > /dev/null 2>&1
+  rm -rf ~/$wdir
 fi  
-mkdir $wdir
-cd $wdir
-echo -e "Téléchargement du jeu de données...."
-curl https://files.slack.com/files-pri/T8AS5C97H-F8CQJNVBN/download/graph.db.zip?pub_secret=f8155bf35e --output graph.db.zip --silent
-echo "Téléchargement de Neo4j..." 
-curl https://files.slack.com/files-pri/T8AS5C97H-F8CTWG77F/download/neo4j-community-2.3.7-unix.tar.gz?pub_secret=fba55c2e20 --output neo4j-community-2.3.7-unix.tar.gz --silent
+mkdir ~/$wdir
 echo -e "Installation de Neo4j..."
-tar xzf neo4j-community-2.3.7-unix.tar.gz
+cat neo4j-community-2.3.7-unix.tar.gz.part-* |gunzip -c > ~/$wdir/neo4j-community-2.3.7-unix.tar.gz
+tar xzf ~/$wdir/neo4j-community-2.3.7-unix.tar.gz -C ~/$wdir
 echo -e "Importation des données dans Neo4j..."
-unzip -qq  graph.db.zip  -d  ./neo4j-community-2.3.7/data
+unzip -qq  graph.db.zip  -d  ~/$wdir/neo4j-community-2.3.7/data
 echo -e "Lancement du serveur Neo4j..."
-sed -i -e "s/\(dbms.security.auth_enabled=\).*/\1false/" ./neo4j-community-2.3.7/conf/neo4j-server.properties
-./neo4j-community-2.3.7/bin/neo4j start > /dev/null 2>&1
+sed -i -e "s/\(dbms.security.auth_enabled=\).*/\1false/" ~/$wdir/neo4j-community-2.3.7/conf/neo4j-server.properties
+~/$wdir/neo4j-community-2.3.7/bin/neo4j start > /dev/null 2>&1
 xdg-open http://localhost:7474  > /dev/null 2>&1 &
 echo -e "L'interface Web est maintenant disponible dans votre navigateur"
-xterm -e "./neo4j-community-2.3.7/bin/neo4j-shell" &
+xterm -e "~/$wdir/neo4j-community-2.3.7/bin/neo4j-shell" &
 echo -e "La shell des commandes Neo4j est maintenant disponible"
-echo  -e "L'installation est finie, bon travail!"
+echo  -e "\n*** L'installation est finie, bon travail! ***\n"
