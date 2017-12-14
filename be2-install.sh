980d7b1025ef8e30803311548cd88330960be169
quercini gianluca <gquercini@gmail.com>
2017-12-14 18:10:30 +0100

    scripts d'installation

--- be2-install.sh
+++ be2-install.sh
@@ -5,6 +5,7 @@
 # Year : 2017 - 2018
 
 
+cwd=$1
 echo -e "\n******\n\nFichier d'installation du BE 2 du cours Big Data.\nAuteur : Gianluca QUERCINI\nAnnée : 2017 - 2018\n\n******\n"
 
 wdir='bigdata-be2'
@@ -15,14 +16,14 @@ if [ -d ~/$wdir ]; then
 fi  
 mkdir ~/$wdir
 echo -e "Installation de Neo4j..."
-cat neo4j-community-2.3.7-unix.tar.gz.part-a* > ~/$wdir/neo4j-community-2.3.7-unix.tar.gz
+cat $cwd/neo4j-community-2.3.7-unix.tar.gz.part-a* > ~/$wdir/neo4j-community-2.3.7-unix.tar.gz
 tar xzf ~/$wdir/neo4j-community-2.3.7-unix.tar.gz -C ~/$wdir
 echo -e "Importation des données dans Neo4j..."
-unzip -qq  graph.db.zip  -d  ~/$wdir/neo4j-community-2.3.7/data
+unzip -qq  $cwd/graph.db.zip  -d  ~/$wdir/neo4j-community-2.3.7/data
 echo -e "Lancement du serveur Neo4j..."
 sed -i -e "s/\(dbms.security.auth_enabled=\).*/\1false/" ~/$wdir/neo4j-community-2.3.7/conf/neo4j-server.properties
-~/$wdir/neo4j-community-2.3.7/bin/neo4j start > /dev/null 2>&1
+nohup ~/$wdir/neo4j-community-2.3.7/bin/neo4j start > /dev/null 2>&1
 echo  "Le serveur est actif"
-read -p "Appuyez sur n'importe quelle touche pour visualiser l'interface Neo4j dans votre navigateur. Bon travail!"
-xdg-open http://localhost:7474  > /dev/null 2>&1 &
-
+nohup xdg-open http://localhost:7474  > /dev/null 2>&1 &
+read -p "L'interface Neo4j est maintenant disponible dans votre navigateur Web $cr 
+Appuyez sur n'importe quelle touche pour fermer cette fenêtre. $cr Bon travail!!"
